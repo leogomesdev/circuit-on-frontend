a9bd74b2b32e39a6d16fb30be866bbb723b321b4
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-11-29 17:14:42 -0500

    feat(propaganda-viewer): set height to 100% for image. Add background color around image

--- src/app/propaganda-viewer/propaganda-viewer.component.ts
+++ src/app/propaganda-viewer/propaganda-viewer.component.ts
@@ -1,43 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-propaganda-viewer',
   templateUrl: './propaganda-viewer.component.html',
   styleUrls: ['./propaganda-viewer.component.css'],
 })
-export class PropagandaViewerComponent {
-  imageSource = '';
+export class PropagandaViewerComponent implements OnInit {
+  imageSource = 'assets/images/placeholder.jpg';
+  backgroundColor = 'white';
+
   apiDataList = [
     {
       title: 'Sprite',
       type: 'IMG_SOURCE',
+      backgroundColor: '#000000',
       data: 'https://www.msureporter.com/wp-content/uploads/2017/04/SpriteWEB.jpg',
-      scheduledTime: '2022-11-29T16:58:00.000Z',
+      scheduledTime: '2022-11-29T22:05:00.000Z',
     },
     {
       title: 'Coca Cola',
       type: 'IMG_SOURCE',
+      backgroundColor: '#FFFF00',
       data: 'http://cdn.differencebetween.net/wp-content/uploads/2019/03/Difference-Between-Advertisement-and-Propaganda--768x529.jpg',
-      scheduledTime: '2022-11-29T16:58:10.000Z',
+      scheduledTime: '2022-11-29T22:05:10.000Z',
     },
     {
       title: '7UP',
       type: 'IMG_SOURCE',
+      backgroundColor: 'red',
       data: 'https://grist.org/wp-content/uploads/2010/07/ad_7up175baby_463x310.jpg',
-      scheduledTime: '2022-11-29T16:58:20.000Z',
+      scheduledTime: '2022-11-29T22:05:20.000Z',
     },
   ];
 
-  constructor() {
+  ngOnInit(): void {
     // @TODO change it after API response
     setTimeout(() => {
       this.scheduleChangesBasedOnAPI();
     }, 3000);
   }
 
-  setImageSource(imageSource: string) {
+  updatePropagandaOnDisplay(imageSource: string, backgroundColor: string) {
     console.log(`Replacing image with ${imageSource}`);
     this.imageSource = imageSource;
+    this.backgroundColor = backgroundColor;
   }
 
   /**
@@ -61,7 +67,10 @@ export class PropagandaViewerComponent {
         )} minutes == image: ${currentValue.title}`
       );
       setTimeout(() => {
-        this.setImageSource(currentValue.data);
+        this.updatePropagandaOnDisplay(
+          currentValue.data,
+          currentValue.backgroundColor
+        );
       }, timeout);
     });
   }
