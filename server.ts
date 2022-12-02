6b110b5a01befc806c8254a729cba7b534284ad9
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-12-02 16:46:19 -0500

    changes for deploy

--- server.ts
+++ server.ts
@@ -11,7 +11,10 @@ import { AppServerModule } from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/propaganda-scheduler/browser');
+  let distFolder = join(process.cwd(), 'browser');
+  if (!existsSync(distFolder)) {
+    distFolder = join(process.cwd(), 'dist/propaganda-scheduler/browser');
+  }
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
