31baf864a0927a6b78442a27a8c111ff83d58aae
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-12-02 16:31:14 -0500

    ci: azure tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: 'view',
     component: PropagandaViewerComponent,
   },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://leodevproplin1.azurewebsites.net/',
+};
