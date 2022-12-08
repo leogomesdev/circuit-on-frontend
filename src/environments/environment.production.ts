9284a0583d0bdaf9a0a524f05c24f8d18adf7183
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-12-08 17:01:34 -0500

    ci: update commands

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   baseUrl: '',
   envVar: {
-      PROPAGANDA_APP_BACKEND_BASE_URL: "https://vsfrvrvnth.us-east-1.awsapprunner.com"
-},
+    PROPAGANDA_APP_BACKEND_BASE_URL:
+      'https://vsfrvrvnth.us-east-1.awsapprunner.com',
+  },
 };
