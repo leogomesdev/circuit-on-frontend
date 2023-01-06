bd226561767dce56b47e7e66e49802e1ffffac29
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2023-01-06 10:42:32 -0500

    fix(environment): fix default value for env config NG_APP_OKTA_REDIRECT_URI

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -7,7 +7,7 @@ export const environment = {
     NG_APP_OKTA_ISSUER: 'https://dev-38659822.okta.com/oauth2/default',
     NG_APP_OKTA_CLIENT_ID: '0oa7uqgniu0Zw9bjn5d7',
     NG_APP_OKTA_REDIRECT_URI:
-      'https://https://t7wfk63ppa.us-east-1.awsapprunner.com/login/callback',
+      'https://t7wfk63ppa.us-east-1.awsapprunner.com/login/callback',
     NG_APP_VIEW_PAGE_FUTURE_ITEMS: 4,
     NG_APP_LIST_PAGE_FUTURE_ITEMS: 15,
   },
