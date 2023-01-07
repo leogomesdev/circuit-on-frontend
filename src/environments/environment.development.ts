356fea298637c0d7f5a443a9d641ed7d087c2ba3
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2023-01-06 19:41:05 -0500

    feat(images): add new categories for images

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
   envVar: {
     NG_APP_PROPAGANDA_APP_BACKEND_BASE_URL: 'http://localhost:3000',
     NG_APP_IMAGE_CATEGORIES:
-      'CrossFit,Zumba,Glute Camp,HIIT Fitness,Sweat -40,CrossFit Kids',
+      'CrossFit,Zumba,Glute Camp,HIIT Fitness,Sweat -40,CrossFit Kids,Private Session,Other',
     NG_APP_OKTA_ISSUER: 'https://dev-38659822.okta.com/oauth2/default',
     NG_APP_OKTA_CLIENT_ID: '0oa7uqgniu0Zw9bjn5d7',
     NG_APP_OKTA_REDIRECT_URI: 'http://localhost:4200/login/callback',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     NG_APP_PROPAGANDA_APP_BACKEND_BASE_URL:
       'https://vsfrvrvnth.us-east-1.awsapprunner.com',
     NG_APP_IMAGE_CATEGORIES:
-      'CrossFit,Zumba,Glute Camp,HIIT Fitness,Sweat -40,CrossFit Kids',
+      'CrossFit,Zumba,Glute Camp,HIIT Fitness,Sweat -40,CrossFit Kids,Private Session,Other',
     NG_APP_OKTA_ISSUER: 'https://dev-38659822.okta.com/oauth2/default',
     NG_APP_OKTA_CLIENT_ID: '0oa7uqgniu0Zw9bjn5d7',
     NG_APP_OKTA_REDIRECT_URI: 'http://localhost:4200/login/callback',
