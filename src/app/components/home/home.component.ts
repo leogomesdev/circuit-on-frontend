0b137f8ce6f41de05c15939adf0189a5d5236535
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2023-01-24 09:19:51 -0500

    fix: fix auth plugin error with AuthSdkError

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { Router } from '@angular/router';
 import { AppPropertiesService } from 'src/app/services/app-properties.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,25 @@ export class HomeComponent {
   constructor(
     private router: Router,
     @Inject(OKTA_AUTH) private oktaAuth: OktaAuth,
+    private messageService: MessageService,
     public app: AppPropertiesService
   ) {}
 
+  /**
+   * Call third-part login integration.
+   * In case of error, force user logout
+   */
   async signIn(): Promise<void> {
     await this.oktaAuth
       .signInWithRedirect()
+      .catch(async (error) => {
+        this.messageService.showError(
+          'Error, please try again',
+          [JSON.stringify(error)],
+          30
+        );
+        await this.oktaAuth.signOut();
+      })
       .then(() => this.router.navigate(['/']));
   }
 }
